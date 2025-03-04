e20ddd3c410de5e31eba34f2da6d944c8b5a449c
Nenthor <david@nentwich.dev>
2025-03-04 22:22:01 +0100

    Added canonical header & more localization

---
+++ src/locales/BasicFrame.ts
@@ -0,0 +1,12 @@
+const translations = {
+  title: {
+    en: `David Nentwich | Portfolio`,
+    de: 'David Nentwich | Portfolio',
+  },
+  description: {
+    en: `Discover the portfolio of David Nentwich - Enthusiastic computer science student`,
+    de: 'Entdecken Sie das Portfolio von David Nentwich - Enthusiastischer Informatikstudent',
+  },
+};
+
+export default translations;
